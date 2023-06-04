48f9f2f3dd06cd4f1bc8dd4131343437b22483a5
Gagandeep <gagandeepkaur1523@gmail.com>
2023-06-04 16:44:34 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/metamask_connect_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.metamask_connect_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:metamask_connect_2/homepage.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
